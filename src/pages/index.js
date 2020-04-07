5e424cbf95ad0294852cdeacd74ff2bdc6fe2e18
Gene Smith <me@genesmith.org>
2020-04-07 16:29:45 -0400

    stylings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
+import axios from 'axios';
+
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
-import axios from 'axios';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -53,7 +54,57 @@ const IndexPage = () => {
         }
       })
     }
-    console.log(geoJson);
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString}
+          </span>
+        `;
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
+
+    geoJsonLayers.addTo(map)
   }
 
   const mapSettings = {
@@ -69,7 +120,7 @@ const IndexPage = () => {
         <title>Home Page</title>
       </Helmet>
 
-      <Map {...mapSettings}></Map>
+      <Map {...mapSettings} />
 
       <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
